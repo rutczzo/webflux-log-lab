39103b3e27aa775df6d5bc4ca06e92d6a5018a97
심심이 <83386922+rutczzo@users.noreply.github.com>
2026-03-14 17:15:18 +0900

    Update load-test.js

--- load-test.js
+++ load-test.js
@@ -1,10 +1,12 @@
 import http from 'k6/http';
 
+url = // todo
+
 export const options = {
     vus: 100,
     duration: '20s',
 };
 
 export default function () {
-    http.get('http://43.200.255.156:8080/test');
+    http.get('http://{url}:8080/test');
 }
